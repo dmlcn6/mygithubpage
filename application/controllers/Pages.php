5f042c0f7e67ed61dac534cf7634b7a11b362313
Tri Come <darryllopez07@gmail.com>
2015-09-16 23:42:17 +0000

    added 1 controller + 4 views

---
+++ application/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php 
+class Pages extends CI_Controller {
+
+	public function view($pages = 'home')
+	{
+		if( !file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+		{
+			//if file doesnt exist at above path
+			show_404();	
+		}
+		
+		$data['title'] = ucfirst($page); //cap
+
+		//load header , $page, and footer
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+	}
+}
---
+++ application/views/templates/header.php
@@ -0,0 +1,6 @@
+<html>
+	<head>
+		<title> Exploration1 CI </title>
+	</head>
+	<body>
+		<h1> <?php echo $title; ?></h1>
