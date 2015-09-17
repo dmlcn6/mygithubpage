8d18a4b593660112dc27ff7a1691e7b27fb5af9c
Tri Come <darryllopez07@gmail.com>
2015-09-17 14:08:13 +0000

    updated Pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -9,6 +9,25 @@ class Pages extends CI_Controller {
 			show_404();	
 		}
 		
+		if ($page == 'myform')
+		{
+		
+            $this->load->helper(array('form', 'url'));
+
+            $this->load->library('form_validation');
+
+            if ($this->form_validation->run() == FALSE)
+            {                        
+            	$this->load->view('templates/header', $data);
+            	$this->load->view('myform');
+            }
+            else
+            {
+               $this->load->view('formsuccess');
+            }
+      	  
+		}
+		
 		$data['title'] = ucfirst($page); //cap
 
 		//load header , $page, and footer
